865f5a4b730660bdbe1524d55760b46b4cd6d97e
melyespinofl96 <melyespinofl96@gmail.com>
2023-05-22 23:23:34 -0500

    update

--- src/app/order/order.component.ts
+++ src/app/order/order.component.ts
@@ -24,39 +24,6 @@ export class OrderComponent implements OnInit{
 
   ngOnInit(): void {
     this.drinksToggle();
-    console.log('before this name: ' + this.Name);
-    //this.Name = this.invoiceService.orderName;
-    console.log('after this name: ' + this.Name);
-
-    /*
-    if(this.invoiceService.getOrder(this.Name) != null){ //if this order exists
-      console.log('inside ngONinit of invoiceComponent');
-      this.Items = this.invoiceService.getOrder(this.Name).items;
-      //console.log(this.invoiceService.getOrder(this.Name).items);
-      //console.log(this.Items)
-      this.TaxAmount = this.invoiceService.getOrder(this.Name).tax;
-      //console.log(this.TaxAmount)
-      this.SubTotal = this.invoiceService.getOrder(this.Name).subtotal;
-      //console.log('subTotal saved: ' + this.invoiceService.getOrder(this.Name).SubTotal)
-      this.Total = this.invoiceService.getOrder(this.Name).total;
-    }
-    */
-  }
-
-  saveButton(){
-
-    this.saveOrder();
-    
-    //this.invoiceService.invoiceToggle(false);
-    /*
-    this.Name = '';
-    this.Items = [];
-    this.SubTotal = 0.00;
-    */
-  }
-
-  setOrderName(name: string):void {
-    this.Name = name
   }
 
   addItem(name: string, price: number): void {
@@ -68,37 +35,16 @@ export class OrderComponent implements OnInit{
     this.Total = this.SubTotal + this.TaxAmount;
   }
 
-  saveOrder(): void {
-    const order = {name: this.Name, items: this.Items, subtotal: this.SubTotal, 
-      tax: this.TaxAmount, total: this.Total}
+  saveButton(){
 
-    console.log('subtotal to be saved is ' + order.subtotal);
-
-    //this.invoiceService.setOrder(this.Name, order);
-
-    console.log('saveOrder() mehtod called from indide invoice component');
-
-    /*
-      const data = { name: 'John', age: 30 };
-      this.localStorageService.setItem('myData', data);
-    */
   }
 
-  getOrder(): any {
-    /*
-    const data = this.invoiceService.getItem('myData');
-    console.log(data);
-    */
+  printButton(){
 
-    //const order = this.invoiceService.getOrder(this.Name);
-    console.log('this is from getOrder() inside invoice component: ');
-    //console.log(this.invoiceService.getOrder(this.Name).name);
-    //return this.invoiceService.getOrder(this.Name);
   }
 
-  removeOrder(): void {
-    //this.invoiceService.removeItem('myData');
-    //this.invoiceService.removeOrder(this.Name);
+  processButton(){
+    
   }
 
   drinksToggle(){
